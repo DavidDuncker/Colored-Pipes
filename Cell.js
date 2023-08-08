064784d40847d6e3475dca3a33c24dd044254357
David Duncker <dave.duncker@gmail.com>
2023-08-08 00:35:09 -0400

    Added functions and classes to PipeDisplay

--- Cell.js
+++ Cell.js
@@ -7,22 +7,37 @@ class Cell {
     bottomBorder = 0;
     pipeDisplay = null;
     canvas = null;
+    isSelected = false;
+    bgcolor = "#f1f1f1";
     
-    constructor(canvas, rowNumber, columnNumber, rowSize, columnSize) {
-        this.canvas = canvas
+    constructor(canvas, rowNumber, columnNumber, rowSize, columnSize, bgcolor) {
+        this.canvas = canvas;
         this.topBorder = rowNumber * rowSize;
         this.bottomBorder = (rowNumber + 1) * rowSize;
-        this.leftBorder = columnNumber * columnSize,
-        this.rightBorder = (columnNumber + 1) * columnSize
-        this.pipeDisplay = new PipeDisplayInsideCell(this, null, null, null, null)
+        this.leftBorder = columnNumber * columnSize;
+        this.rightBorder = (columnNumber + 1) * columnSize;
+        this.pipeDisplay = new PipeDisplayInsideCell(this, null, null, null, null);
+        this.bgcolor = "#f1f1f1";
     }
 
     drawPipe(direction1, direction2, color) {
+
         this.pipeDisplay.drawPipeEdge(direction1, color);
         this.pipeDisplay.drawPipeEdge(direction2, color);
         this.pipeDisplay.drawPipeCenter(direction1, direction2, color)
 
     }
+
+    makeSelected() {
+        this.isSelected = true;
+        this.pipeDisplay.drawSolidColor("black")
+    }
+
+    makeUnselected() {
+        this.isSelected = false;
+        this.pipeDisplay.drawSolidColor("white")
+    }
+
 
 }
 
